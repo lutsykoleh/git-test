54e9b4380635251eb30b7520acc8d9d25628bb3f
olegl <lutsyk.oleh00@gmail.com>
2024-05-30 11:09:33 +0300

    added hook

---
+++ example.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace VendorName;
+
+class Example
+{
+    public function exampleFunction()
+    {
+        $array = [1, 2, 3];
+        foreach ($array as $value) {
+            echo $value;
+        }
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2aa58eb4919147ef470bac73ed068798de3187a8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2aa58eb4919147ef470bac73ed068798de3187a8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'squizlabs/php_codesniffer' => array(
+            'pretty_version' => '3.10.1',
+            'version' => '3.10.1.0',
+            'reference' => '8f90f7a53ce271935282967f53d0894f8f1ff877',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../squizlabs/php_codesniffer',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
